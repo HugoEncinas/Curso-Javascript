6f76679e6bcc1042d06aea3f87a9e8260035e80d
HugoEncinas <hugo.encinas@bmtask.com>
2017-05-18 23:31:04 -0500

    Acceleration

---
+++ Coding Math/main_vector.js
@@ -0,0 +1,22 @@
+var v1 = vector.create(10, 5),
+    v2 = vector.create(3, 4),
+    v3 = v1.add(v2),
+    v4 = v1.multiply(2);
+
+// console.log(v1.getX());
+// console.log(v1.getY());
+// console.log(v1.getAngle());
+// console.log(v1.getLength());
+//
+// v1.setAngle(Math.PI / 6);
+// v1.setLength(100);
+//
+// console.log(v1.getX());
+// console.log(v1.getY());
+
+// console.log(v3.getX(), v3.getY());
+// console.log(v1.getLength());
+// console.log(v4.getLength());
+
+// v1.addTo(v2);
+// console.log(v1.getX(), v1.getY());
