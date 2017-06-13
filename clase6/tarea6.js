9d67dbf0f8c5df07ba74532730851475e3e28113
HugoEncinas <hugomty93@outlook.com>
2017-06-13 15:51:02 -0500

    tarea 6

--- clase6/tarea6.js
+++ clase6/tarea6.js
@@ -7,32 +7,40 @@ window.onload = function() {
     max: true
   });
 
-
-
   var firstName = document.getElementById('firstName');
   var lastNameP = document.getElementById('lastNameP');
   var lastNameM = document.getElementById('lastNameM');
   var picker = $input.pickadate('picker');
+  var pickerModal = document.getElementsByClassName('picker__holder');
   var curp = document.getElementById('curp');
-  firstName.addEventListener('keyup', copy);
-  var badWords = ['culo', 'loco', 'wuey', 'roba'];
+  curpForm.addEventListener('keyup', copy);
+  curpForm.addEventListener('click', copy);
+  curpForm.addEventListener('submit', generateObject);
+  pickerModal[0].addEventListener('click', copy);
+  var badWords = ['culo', 'loco', 'wuey', 'roba', 'pene'];
   var grosero = false;
-  // var male = document.getElementById("male").checked;
-  // var female = document.getElementById("female").checked;
-  var pregnant = document.getElementById("pregnant");
+  var pregnant = "no"
+  var year="", month="", date="";
+  var gender="";
+  var state = document.getElementById("state");
+  var stateText = "";
+  var curpTextArea = document.getElementById("curpTextArea");
+  var curpObject;
 
   //Curp character values
-  var c1, c2, c3, c4, c5, c6, c7, c8, c9, c10;
+  var c1="?", c2="?", c3="?", c4="?", c5="??", c6="??", c7="??", c8="?", c9="??", c10="?", c11="?", c12="?";
+  var curpString;
 
+  //event listener for the gender checkbox
   $("input[name='gender']").on('click', function(e) {
       if (e.currentTarget.id=="female"){
          $('#pregnant').prop("disabled", false);
-         c8 = "M";
+         gender="female"
       }
       else{
         $('#pregnant').prop("disabled", true);
         $('#pregnant').prop("checked", false);
-        c8 = "H";
+        gender="male"
       }
   });
 
@@ -68,82 +76,105 @@ window.onload = function() {
     {text: "Yucatán", value: "YN"},
     {text: "Zacatecas", value: "ZS"}
   ];
-  var sel = document.getElementById('state');
+  //dynamically populates the select input with the states array
   for (var i = 0; i < states.length; i ++) {
     var option = document.createElement('option');
     option.setAttribute('value', states[i].value);
     option.appendChild(document.createTextNode(states[i].text));
-    sel.appendChild(option);
+    state.appendChild(option);
   }
 
-
+  //main function that updates the CURP value
   function copy() {
-    c1 = lastNameP.value.substring(0,1);
-    c2 = getFirstVowel(lastNameP.value);
-    c3 = lastNameM.value.substring(0,1);
-    c4 = firstName.value.substring(0,1);
-    c5 = picker.get('select').year;
-    c6 = picker.get('select').month+1; //Plus 1 becuase months have zero-as-index
-    c7 = picker.get('select').date;
-    console.log(c8);
-    curp.value = firstName.value;
+    c1 = lastNameP.value.substring(0,1).toUpperCase();
+    c2 = getFirstVowel(lastNameP.value).toUpperCase();
+    c3 = lastNameM.value.substring(0,1).toUpperCase();
+    c4 = firstName.value.substring(0,1).toUpperCase();
+    if (picker.get('select')!=null){
+      year = picker.get('select').year;
+      month = picker.get('select').month+1; //Plus 1 becuase months have zero-as-index
+      date = picker.get('select').date
+      c5 = String(year).slice(-2);
+      c6 = month.toString();
+      c7 = date.toString();
+    }
+    if (c6.length == 1) {
+      c6 = "0" + c6;
+    }
+    if (c7.length == 1) {
+      c7 = "0" + c7;
+    }
+    if (gender=="male"){
+      c8 = "H";
+    }
+    else if(gender=="female"){
+      c8 = "M";
+    }
+    c9 = state.options[state.selectedIndex].value;
+    console.log(state.selectedIndex);
+    if (c9 == "") {
+      c9 = "??";
+    }
+    c10 = getFirstConsonant(lastNameP.value).toUpperCase();
+    c11 = getFirstConsonant(lastNameM.value).toUpperCase();
+    c12 = getFirstConsonant(firstName.value).toUpperCase();
+
+    // console.log(c1+c2+c3+c4+c5+c6+c7+c8+c9+c10+c11+c12);
+    curpString = c1+c2+c3+c4+c5+c6+c7+c8+c9+c10+c11+c12;
+
+    //Replaces the second letter of a potential censored word with an X
     badWords.forEach(function (word) {
-      if(firstName.value.includes(word)) {
-        curp.value = 'Grosero' //Aqui remplazar una de las letras por X
+      if(curpString.substring(0,4).toLowerCase().includes(word)) {
+        curpString = curpString.substr(0, 1) + 'X' + curpString.substr(2);
       }
     })
-  }
-
-  function check() {
-    console.log(gender);
+    curp.value = curpString;
   }
 
   function getFirstVowel(word){
     for(var x = 1; x < word.length; x++){
       if(['a', 'e', 'i', 'o', 'u'].indexOf(word[x].toLowerCase()) !== -1) { return word[x]}
     }
-    return 'x';
+    return '?';
   }
 
   function getFirstConsonant(word){
     for(var x = 1; x < word.length; x++){
       if(['a', 'e', 'i', 'o', 'u'].indexOf(word[x].toLowerCase()) == -1) { return word[x]}
     }
-    return 'x';
+    return '?';
+  }
+
+  function generateObject(e){
+    e.preventDefault(); //Prevents reloading the page
+    if (document.getElementById('pregnant').checked){
+      pregnant = "Yes"
+    }
+    else {
+      pregnant = "No"
+    }
+    if (state.selectedIndex!=0) {
+      stateText = states[state.selectedIndex-1].text;
+    }
+    console.log("entro");
+    curpObject = {
+      "name":{
+        "first name": firstName.value,
+        "paternal last name": lastNameP.value,
+        "maternal last name": lastNameM.value,
+      },
+      "birthday":{
+        "year": year,
+        "month": month,
+        "day": date
+      },
+      "gender": gender,
+      "pregnant": pregnant,
+      "state": stateText
+    };
+    curpObject = JSON.stringify(curpObject,undefined, 2);
+    curpTextArea.value=curpObject;
+    Materialize.updateTextFields();
+    $('#curpTextArea').trigger('autoresize');
   }
 };
-
-//Tarea: EIVH930510HNLNLG05
-// 'Mi super generador de Curp', que se calcule al momento, cambiar palabras mala por X (pene, pxne), minimo 5 palabras
-// Inputs: Nombre, Apellido, etc.
-// Logica:
-// Primer letra Ap. paterno
-// Primer vocal ap. paterno
-// Primer letra Ap. materno
-// Primer letra nombre
-// año 80, 95
-// mes 02, 03
-// dia 10, 20
-// sexo h, m (radio button)
-// checkbox opcional de embarazada?, en caso de ser mujer
-// Entidad nacimiento SR, NL  (Select box)
-// Primera consonante ap. paterno (no inicial)
-// Primera consonante ap. materno (no inicial)
-// Primera consoante nombre (no incial)
-
-//Parte 2
-//Ademas de lo anterior,
-//boton de enviar
-// en textArea, imprimir json del objeto que hemos generado, en ingles
-// {
-//   name: {
-//     name:"",
-//     lastName:"",
-//     lastName2:""
-//   },
-//   DOB: {
-//     day,
-//     month,
-//     year
-//   }
-// }
